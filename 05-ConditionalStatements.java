4161d3f87fbf73561780de94dff5a418d59f38c6
shreyakumud <shreya050426@gmail.com>
2026-09-08 00:36:22 +0530

    Add switch statement example

--- 05-ConditionalStatements.java
+++ 05-ConditionalStatements.java
@@ -13,7 +13,7 @@ public class ConditionalStatements {
       System.out.println("You are an adult.");
     } else {
       System.out.println("You are a minor.")
-        }
+    }
 
     //else-iF
     System.out.println("Enter your marks: ");
@@ -39,9 +39,66 @@ public class ConditionalStatements {
       }
     }
 
+    System.out.print("Enter a day number (1-7);
+    int day = sc.nextInt();
+
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thrusday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Invalid day");
+    }
+
     sc.close();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+    
+
+   
 
 
 
