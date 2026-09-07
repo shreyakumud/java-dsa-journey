a3a5bf7ad64645dabd0ca0e34c9398ee1771cb58
shreyakumud <shreya050426@gmail.com>
2026-09-07 23:22:36 +0530

    Add variables and data types example

---
+++ 02-VariablesAndDataTypess.java
@@ -0,0 +1,18 @@
+public class VariablesAndDataTypes {
+  public static void main(String[] args) {
+    
+    int age = 19;
+    double heaight = 5.3;
+    char grade = 'A';
+    boolean isStudent = true;
+    String name = "Shreya";
+
+    System.out.println("Nmae: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height);
+    System.out.println("Grade: " + grade);
+    System.out.println("Student: " + isStudent);
+  }
+}
+                       
+      
