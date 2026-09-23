0f8f477f439f5ca4de478f0d7b9c285d24d300d0
shreyakumud <shreya050426@gmail.com>
2026-09-23 18:03:26 +0530

    Add Java pattern programs 5-11

--- 08-Patterns.java
+++ 08-Patterns.java
@@ -62,6 +62,141 @@ public class Patterns {
     sc.close();
   }
 }
+
+// Pattern 5: 180 Degree Rotated Half Pyramid
+
+System.out.print("Enter the number of rows: ");
+int n5 = sc.nextInt();
+
+for (int i = 1; i <= n5; i++) {
+
+    for (int j = 1; j <= n5 - i; j++) {
+        System.out.print("  ");
+    }
+
+    for (int j = 1; j <= i; j++) {
+        System.out.print("* ");
+    }
+
+    System.out.println();
+}
+
+
+// Pattern 6: Number Triangle
+
+System.out.print("Enter the number of rows: ");
+int n6 = sc.nextInt();
+
+for (int i = 1; i <= n6; i++) {
+
+    for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+    }
+
+    System.out.println();
+}
+
+
+// Pattern 7: 0-1 Triangle
+
+System.out.print("Enter the number of rows: ");
+int n7 = sc.nextInt();
+
+for (int i = 1; i <= n7; i++) {
+
+    for (int j = 1; j <= i; j++) {
+
+        if ((i + j) % 2 == 0) {
+            System.out.print("1 ");
+        } else {
+            System.out.print("0 ");
+        }
+    }
+
+    System.out.println();
+}
+
+
+// Pattern 8: Number Rectangle
+
+System.out.print("Enter the number of rows: ");
+int rows8 = sc.nextInt();
+
+System.out.print("Enter the number of columns: ");
+int cols8 = sc.nextInt();
+
+for (int i = 1; i <= rows8; i++) {
+
+    for (int j = 1; j <= cols8; j++) {
+        System.out.print(j + " ");
+    }
+
+    System.out.println();
+}
+
+// Pattern 9: Floyd's Triangle
+
+System.out.print("Enter the number of rows: ");
+int n9 = sc.nextInt();
+
+int number = 1;
+
+for (int i = 1; i <= n9; i++) {
+
+    for (int j = 1; j <= i; j++) {
+        System.out.print(number + " ");
+        number++;
+    }
+
+    System.out.println();
+}
+
+// Pattern 10: 0-1 Rectangle
+
+System.out.print("Enter the number of rows: ");
+int rows10 = sc.nextInt();
+
+System.out.print("Enter the number of columns: ");
+int cols10 = sc.nextInt();
+
+for (int i = 1; i <= rows10; i++) {
+
+    for (int j = 1; j <= cols10; j++) {
+
+        if ((i + j) % 2 == 0) {
+            System.out.print("1 ");
+        } else {
+            System.out.print("0 ");
+        }
+    }
+
+    System.out.println();
+}
+
+// Pattern 11: Palindromic Number Pyramid
+
+System.out.print("Enter the number of rows: ");
+int n11 = sc.nextInt();
+
+for (int i = 1; i <= n11; i++) {
+
+    // Spaces
+    for (int j = 1; j <= n11 - i; j++) {
+        System.out.print("  ");
+    }
+
+    // Descending numbers
+    for (int j = i; j >= 1; j--) {
+        System.out.print(j + " ");
+    }
+
+    // Ascending numbers
+    for (int j = 2; j <= i; j++) {
+        System.out.print(j + " ");
+    }
+
+    System.out.println();
+}
     
       
     
